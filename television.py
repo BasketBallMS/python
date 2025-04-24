395be6c3864e7e936f7c924d1104ead0aa827876
BasketBallMS <gravesm221@gmail.com>
2025-04-24 16:29:09 -0500

    works

--- television.py
+++ television.py
@@ -1,38 +1,59 @@
 class Television:
+    """
+    A class representing a basic Television with power, volume, channel, and mute functionality
+    """
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Initializes a Television objects with default values
+        """
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
         self.__prev_volume = self.__volume
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Turns the Television on or off
+        """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self)-> None:
+        """
+        sets the volume to 0
+        """
         self.__muted = not self.__muted
         self.__volume = 0
 
-    def channel_up(self):
+    def channel_up(self)-> None:
+        """
+        Brings the channel up in increments of 1
+        """
         if self.__status:
             if self.__channel < self.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = self.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self)-> None:
+        """
+        Brings the channel down in increments of 1
+        """
         if self.__status:
             if self.__channel > self.MIN_CHANNEL:
                 self.__channel -= 1
             else:
                 self.__channel = self.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self)-> None:
+        """
+        Brings the volume up in increments of 1
+        """
         if self.__status:
             if self.__muted:
                 self.__volume = self.__prev_volume
@@ -43,7 +64,10 @@ class Television:
             else:
                 self.__volume = self.MAX_VOLUME
 
-    def volume_down(self):
+    def volume_down(self)-> None:
+        """
+        Brings the volume down in increments of 1
+        """
         if self.__status:
             if self.__muted:
                 self.__volume = self.__prev_volume
@@ -54,8 +78,20 @@ class Television:
             else:
                 self.__volume = self.MIN_VOLUME
 
-    def __str__(self):
+    def __str__(self)-> str:
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+
+    def get_volume(self):
+        return self.__volume
+
+    def get_channel(self):
+        return self.__channel
+
+    def get_is_power_on(self):
+        return self.__status
+
+    def get_is_muted(self):
+        return self.__muted
 
 
 
---
+++ test_television.py
@@ -0,0 +1,64 @@
+from pytest import *
+from television import Television
+
+def test___init__():
+    tv = Television()
+    assert tv.get_is_power_on() == False
+    assert tv.get_is_muted() == False
+    assert tv.get_channel() == 0
+    assert tv.get_volume() == 0
+
+def test_power():
+    tv = Television()
+    tv.power()
+    assert tv.get_is_power_on() == True
+    tv.power()
+    assert tv.get_is_power_on() == False
+
+def test_mute():
+    tv = Television()
+    tv.mute()
+    assert tv.get_is_muted() == True
+    tv.mute()
+    assert tv.get_is_muted() == False
+
+def test_channel_up():
+    tv = Television()
+    tv.power()
+    current_channel = tv.get_channel()
+    tv.channel_up()
+    expected_channel = current_channel + 1 if current_channel < tv.MAX_CHANNEL else tv.MIN_CHANNEL
+    assert tv.get_channel() == expected_channel
+
+def test_channel_down():
+    tv = Television()
+    tv.power()
+    current_channel = tv.get_channel()
+    tv.channel_down()
+    expected_channel = current_channel - 1 if current_channel > tv.MIN_CHANNEL else tv.MAX_CHANNEL
+    assert tv.get_channel() == expected_channel
+
+def test_volume_up():
+    tv = Television()
+    tv.power()
+    current_volume = tv.get_volume()
+    tv.volume_up()
+    expected_volume = current_volume + 1  if current_volume < tv.MAX_VOLUME else tv.MAX_VOLUME
+    assert tv.get_volume() == expected_volume
+
+def test_volume_down():
+    tv = Television()
+    tv.power()
+    current_volume = tv.get_volume()
+    tv.volume_down()
+    expected_volume = current_volume - 1 if current_volume - 1 > tv.MIN_VOLUME else tv.MIN_VOLUME
+    assert tv.get_volume() == expected_volume
+
+def test___str__():
+    tv = Television()
+    expect_output = "Power = False, Channel = 0, Volume = 0"
+    assert str(tv) == expect_output
+    tv.power()
+    tv.channel_up()
+    tv.volume_up()
+    assert str(tv) == f'Power = True, Channel = {tv.get_channel()}, Volume = {tv.get_volume()}'
